3d9765ba64d77e41cba5683a02b22cff3ae56aa4
pablommm <murgiapablo@gmail.com>
2023-05-21 18:13:50 -0300

    agrego comments y una matriz ya hecha para no estar ingresando datos siempre

--- LU.py
+++ LU.py
@@ -1,11 +1,16 @@
 # factorizacion de LU
 import math
+import time
 import numpy as np
 
-print ("metodo de LU para matrices cuadradas")
-
-m=int(input("indique el numero de columnas/filas"))
-
+print("\n---------------------------------------------")
+print ("*********metodo de LU para matrices cuadradas*********")
+print("\n---------------------------------------------")
+"""
+m=int(input("Indique el numero de columnas/filas: "))
+print("\n---------------------------------------------")
+"""
+m=3
 matriz= np.zeros([m,m]) # defino la matriz
 
 u=np.zeros([m,m])
@@ -15,9 +20,13 @@ print("ingrese los elementos de la matriz")
 # r es fila/renglon c es columna
 for r in range(0,m):
     for c in range(0,m):
-        matriz [r,c]=(input("elemento a["+str(r+1)+","+str(c+1)+"]"))
+        matriz [r,c]=(input("elemento a["+str(r+1)+","+str(c+1)+"]: "))
         matriz [r,c]=float(matriz[r,c])
         u[r,c]=matriz[r,c]
+
+
+#u = [[ 2, -1,  1.],[ 3,  3,  9.],[ 3,  3,  5]]
+time.sleep(1)
 
 #operacion para poner ceros debajo de la diagonal
 for k in range(0,m):
@@ -27,16 +36,19 @@ for k in range(0,m):
         if (k<r):
             factor=(matriz[r,k]/matriz[k,k])
             l[r,k]=factor
+            
             for c in range(0,m):
                 matriz[r,c]=matriz[r,c]-(factor*matriz[k,c])
                 u[r,c]=matriz[r,c]
-
+                
 print("resultados")
 print("matriz l")
 print(l)
+print("\n---------------------------------------------")
 print("resultados")
 print("matriz u")
 print(u)
+print("\n---------------------------------------------")
 
 #comprobacion
 matrizr=np.zeros([m,m])
@@ -46,5 +58,6 @@ for r in range (0,m):
             matrizr[r,c] +=(l[r,k]*u[k,c])
 print(matrizr)
 a=np.dot(l,u)
+print("\n---------------------------------------------")
 print(a)                
 
