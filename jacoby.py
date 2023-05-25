6b3d07dfb48fc3e80ddff8c20f61c204f4d0ade6
pablommm <murgiapablo@gmail.com>
2023-05-25 00:29:25 -0300

    yes

--- jacoby.py
+++ jacoby.py
@@ -1,4 +1,3 @@
-# Importando Numpy
 import numpy as np
 import pprint
 
@@ -12,16 +11,43 @@ Adicionalmente recibe un vector inicial  x, la estimación del error   e y el m�
 función utiliza dentro de un ciclo, la función jacobi definida anteriormente. Entrega el vector x calculado y el número 
 de iteraciones realizadas k. Si el método no converge, x contendrá un vector nulo.
 
+resumen:
 "a" es la matriz de coeficientes que recibe
-"b" es el vector de constantes de un sistema lienal
+"b" es el vector de constantes de un sistema lineal
 "x" es el vector inicial
 "e" es la estimacion del error
 "m" es el maximo de iteracion permitidas
 "k" es el numero de interaciones realizadas
-"jacobi" entrega el vector "x" ya calculado y "k" las iteraciones
+"jacobi" entrega el vector "x" ya calculado y las iteraciones "k"
 
 si el metodo converge "x" sera nulo 
 '''
+m=3
+matriz,A,b= np.zeros([m,m]) # defino las matrices
+
+#basandome en este video https://www.youtube.com/watch?v=Q6BdR45uo7I&t=670s
+# Matriz de 3x3 para hacer pruebas
+#A = np.array([[3,-1,-1],[-1,3,1],[2,1,4],],float)
+print("Porfavor ingrese los elementos de la matriz a trabajar con Jacobi\n")
+for r in range(0,m):
+    for c in range(0,m):
+        matriz [r,c]=(input("elemento a["+str(r+1)+","+str(c+1)+"]: "))
+        matriz [r,c]=float(matriz[r,c])
+        A[r,c]=matriz[r,c]
+
+# Vector Solución para hacer pruebas 
+#b = np.array([[1],[3],[7],],float)
+print("Porfavor ingrese los elementos del vector solucion\n")
+for c in range(0,m):
+        matriz [r,c]=(input("elemento a["+str(r+1)+","+str(c+1)+"]: "))
+        matriz [r,c]=float(matriz[r,c])
+        b[r,c]=matriz[r,c]
+        
+# Vector de Inicio
+x = np.array([[0],[0],[0]],float)
+
+# Numéro de iteraciones maximo antes de arrojar error
+maxite=1000
 
 def jacobi(a,b,x): 
 	n=len(x) 
@@ -33,7 +59,6 @@ def jacobi(a,b,x):
 				s=s+a[i,j]*t[j]
 				x[i]=(b[i]-s)/a[i,i]
 	return x
-
 
 def jacobim(a,b,x,e,m): 
 	n=len(x)  
@@ -47,33 +72,6 @@ def jacobim(a,b,x,e,m):
 		else:
 			t=x.copy() 
 	return [[],m]
-
-# Matriz a usar
-"""
-A = np.array([[10,-1,2,0],
-			[-1,11,-1,3],
-			[2,-1,10,-1],
-			[0,3,-1,8]],float)
-"""
-#basndome en este video https://www.youtube.com/watch?v=Q6BdR45uo7I&t=670s
-
-A = np.array([[3,-1,-1],
-			[-1,3,1],
-			[2,1,4],
-],float)
-
-
-# Vector Solución
-#b = np.array([[6],[25],[-11],[15]],float)
-
-b = np.array([[1],[3],[7],],float)
-
-# Vector de Inicio
-#x=np.array([[0],[0],[0]],float)
-x=np.array([[0],[0],[0]],float)
-
-# Numéro de iteraciones
-maxite=1000
 
 print ("\nMatriz A:")
 pprint.pprint(A)
