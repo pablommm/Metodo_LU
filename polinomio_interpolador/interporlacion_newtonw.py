b877e769516a93af0a64e7b727e9bcc2a44fd1bc
pablommm <murgiapablo@gmail.com>
2023-06-04 00:08:01 -0300

    yes

--- polinomio_interpolador/interporlacion_newtonw.py
+++ polinomio_interpolador/interporlacion_newtonw.py
@@ -12,11 +12,19 @@ print("\n---------------------------------------------")
 print("Debemos geneara 20 numeros al azar")
 maximo=int(input("Indique el valor maximo que podra tomar: "))
 minimo=int(input("Indique ahora el valor minimo que podra tomar: "))
+
+
 lista_de_numeros = ([])
 print("\n---------------------------------------------")
 
 for i in range (20):
-    lista_de_numeros.append(random.randrange(minimo,maximo))
-
-print("generemos la siguiente lista de numeros aleatorios")
-print(lista_de_numeros)    
+    punto=([])
+    for r in range(2):
+        n=random.randrange(minimo,maximo)
+        punto.append(n)
+        print("agregue el valor ",n)
+        print("punto ", punto)
+    lista_de_numeros.append(punto)
+        
+print("asi me quedo la lista de vectoes ",lista_de_numeros)
+         
