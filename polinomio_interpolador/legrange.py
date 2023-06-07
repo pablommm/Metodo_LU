0f1cc0547f2e5370d99bd621bcd0a98ec00d8984
pablommm <murgiapablo@gmail.com>
2023-06-06 22:34:01 -0300

    yes

--- polinomio_interpolador/legrange.py
+++ polinomio_interpolador/legrange.py
@@ -8,9 +8,9 @@ import matplotlib.pyplot as plt
 import sympy as sym
 
 print("\n---------------------------------------------")
-print ("*********Polinomio interpolador de Newtown*********")
+print ("*********Polinomio interpolador de Legrange*********")
 print("\n---------------------------------------------")
-
+"""
 print("Debemos geneara 20 numeros al azar")
 maximo=int(input("Indique el valor maximo que podra tomar: "))
 minimo=int(input("Indique ahora el valor minimo que podra tomar: "))
@@ -33,7 +33,9 @@ for i in range (20):
         print("agregue el valor ",n)
         print("punto ", punto)
     lista_de_numeros.append(punto)
-        
+
+"""  
+lista_de_numeros = [[9, 15], [17, 17], [5, 14], [12, 11], [14, 9]] 
 print("Asi quedo la lista de vectores ",lista_de_numeros)
 
 valores_en_x = []
@@ -63,8 +65,8 @@ for i in range(len(lista_de_numeros)):
             #print("El numerador va tomando el valor: ",numerador)
             denominador = denominador * (xi[i]- xi[j])
             #print("El denominador va tomando el valor: ",numerador)
-        L = numerador/denominador
-        polinomio = polinomio + L * fi[i] # aqui vamos almacenando las partes del polinomio que se van almacenando
+    L = numerador/denominador
+    polinomio = polinomio + L * fi[i] # aqui vamos almacenando las partes del polinomio que se van almacenando
         
 
 #print("El polinomio con legrange sin simplificar es ",polinomio)
@@ -81,7 +83,7 @@ print("El valor minimo de x es: ",a)
 b= max(xi)
 print("El valor maximo de x es: ",b)
 
-pxi = np.linspace(a,b,50)
+pxi = np.linspace(a,b,200)
 pyi = pol(pxi)# los valores de Y son los valores en X valuados en el polinomio
 
 plt.figure()
