40a9ffe29799aa134fe23d69a1962cacdf74ec52
pablommm <murgiapablo@gmail.com>
2023-06-06 22:34:39 -0300

    yes

--- polinomio_interpolador/legrange.py
+++ polinomio_interpolador/legrange.py
@@ -10,7 +10,7 @@ import sympy as sym
 print("\n---------------------------------------------")
 print ("*********Polinomio interpolador de Legrange*********")
 print("\n---------------------------------------------")
-"""
+
 print("Debemos geneara 20 numeros al azar")
 maximo=int(input("Indique el valor maximo que podra tomar: "))
 minimo=int(input("Indique ahora el valor minimo que podra tomar: "))
@@ -34,8 +34,8 @@ for i in range (20):
         print("punto ", punto)
     lista_de_numeros.append(punto)
 
-"""  
-lista_de_numeros = [[9, 15], [17, 17], [5, 14], [12, 11], [14, 9]] 
+ 
+#lista_de_numeros = [[9, 15], [17, 17], [5, 14], [12, 11], [14, 9]] 
 print("Asi quedo la lista de vectores ",lista_de_numeros)
 
 valores_en_x = []
