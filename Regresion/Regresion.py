deae624ac75759abc2aa5a8a66cec9ffebe9f238
Nicolas Gonzalez <38365041+porwaltz@users.noreply.github.com>
2023-06-26 23:52:28 -0300

    Update Regresion.py

--- Regresion/Regresion.py
+++ Regresion/Regresion.py
@@ -5,12 +5,17 @@ from scipy.stats import pearsonr
 import time
 import sympy as sym
 import numpy as np
+import math
 
 #print(os.getcwd())
 data = pd.read_csv('./Regresion/ACUMULADOS_vs_DIAS.csv')
 #excel = pd.read_excel('./Regresion/ACUMULADOS_vs_DIAS.xlsx')
-print(len(data))
 
+
+
+
+
+#LINEAL
 n = len(data)
 exi = sum(data.dia)
 eyi = sum(data.acumulados)
@@ -22,8 +27,6 @@ sigma_xi_yi = 0
 for i in range(len(data.dia)):
     sigma_xi_yi += (data.dia[i]*data.acumulados[i])
 
-
-
 def obtener_a(df1, df2):
     n= len(df1)
     exi = sum(df1)
@@ -32,8 +35,6 @@ def obtener_a(df1, df2):
     sigma_xi_yi = 0
 
     for i in range(len(df1)):
-        current1 = int(df1[i])
-        current2 = int(df2[i])
         sigma_xi_yi += (df1[i]*df2[i])
         sigma_xi2 += (df1[i]*df1[i])
 
@@ -52,22 +53,38 @@ def obtener_b(df1, df2, a):
     return b
 
 def rse_lineal(df1, df2, a, b):
-    exi = sum(df1)
-    eyi = sum(df2)
     rse = 0
     for i in range(len(df1)):
         rse += (a*df1[i] + b - df2[i])**2
+    return rse
+
+def rse_polinomico(df1, df2, a, b):
+    rse = 0
+    for i in range(len(df1)):
+        rse += (b * df1[i] ** a - df2[i])**2
+    return rse
+
+def rse_exponencial(df1, df2, a, b):
+    rse = 0
+    for i in range(len(df1)):
+        #b*e**ax - Y
+        rse += (b * math.e ** (a * df1[i]) - df2[i])**2
     return rse
 
 
 
 
 
+
+
+print("----------------------------------------------------------")
+print("------------------MODELO LINEAL---------------------------")
+print("----------------------------------------------------------")
 a_obtenido = obtener_a(data.dia, data.acumulados)
 print("El valor de a es: ", a_obtenido )
 b_obtenido = obtener_b(data.dia, data.acumulados, a_obtenido)
 print("El valor de b es: ", b_obtenido)
-recta = "y = " + str(a_obtenido) + "*x + " + str(b_obtenido)
+recta = "y = " + str(a_obtenido) + " * x + " + str(b_obtenido)
 print("La recta obtenida es: ", recta)
 error_cuadrado = rse_lineal(data.dia, data.acumulados, a_obtenido, b_obtenido)
 print("El error obtenido es: ", error_cuadrado)
@@ -75,39 +92,77 @@ print("El error obtenido es: ", error_cuadrado)
 x = sym.Symbol("x")
 funcion_1 = lambda x: 1406.5942618467504*x -62138.47083685545
 
-def calcular_r(x, y):
-    """ std_x = np.std(x)
-    std_y = np.std(y)
-    xy = 0
-    for i in range(len(x)):
-        xy += x[i]*y[i]
-    sum_x = sum(x)
-    sum_y = sum(y)
-    numerador = xy - (sum_x * sum_y)
-    denominador = std_x * std_y
-    r = numerador / denominador """
-
-    n = len(x)
-    sigma_xi = sum(x)
-    sigma_yi = sum(y)
-    sigma_xi2 = 0
-    sigma_xi_yi = 0
-
-    for i in range(len(x)):
-        sigma_xi_yi += (x[i]*y[i])
-        sigma_xi2 += (x[i]*x[i])
-
-    numerador = (n * sigma_xi_yi) - (sigma_xi * sigma_yi)
-    denominador = 1
-    r = 0
-
-    return r
-
 #r_obtenido = calcular_r(data.acumulados, funcion(data.dia))
 r_obtenido, _ = pearsonr(data.acumulados, funcion_1(data.dia))
 
-print("El r obtenido es de: ", r_obtenido)
+print("El r obtenido es de: ", r_obtenido, "\n")
+time.sleep(2)
 
+
+print("----------------------------------------------------------")
+print("------------------MODELO POLINOMICO-----------------------")
+print("----------------------------------------------------------")
+
+def regresion_polinomica(x, y):
+
+    # Perform the least squares regression
+    log_x = np.log(x)
+    log_y = np.log(y)
+    coefficients = np.polyfit(log_x, log_y, 1)
+    a = coefficients[0]
+    b = np.exp(coefficients[1])
+
+    return a, b
+
+a_polinomico, b_polinomico = regresion_polinomica(data.dia, data.acumulados)
+
+funcion_2 = lambda x: b_polinomico * x ** a_polinomico
+r_obtenido_polinomico, _ = pearsonr(data.acumulados, funcion_2(data.dia))
+
+
+print("El valor de a es: ", a_polinomico )
+print("El valor de b es: ", b_polinomico)
+recta_polinomica = "y = " + str(b_polinomico) + " * x ** " + str(a_polinomico)
+print("La recta obtenida es: ", recta_polinomica)
+error_cuadrado_polinomico = rse_polinomico(data.dia, data.acumulados, a_polinomico, b_polinomico)
+print("El error obtenido es: ", error_cuadrado)
+print("El r obtenido es de: ", r_obtenido_polinomico, "\n")
+time.sleep(2)
+
+
+
+print("----------------------------------------------------------")
+print("------------------MODELO EXPONENCIAL----------------------")
+print("----------------------------------------------------------")
+
+def regresion_exponencial(x , y):
+
+    # Perform the least squares regression
+    coefficients = np.polyfit(x, np.log(y), 1)
+    a = coefficients[0]
+    b = np.exp(coefficients[1])
+
+    return a, b
+
+a_exponencial, b_exponencial = regresion_exponencial(data.dia, data.acumulados)
+
+funcion_3 = lambda x: b_exponencial * math.e** (a_exponencial * x)
+r_obtenido_exponencial, _ = pearsonr(data.acumulados, funcion_3(data.dia))
+
+print("El valor de a es: ", a_exponencial )
+print("El valor de b es: ", b_exponencial)
+recta_exponencial = "y = " + str(b_exponencial) + " * e ** (" + str(a_polinomico) + " * x)"
+print("La recta obtenida es: ", recta_exponencial)
+error_cuadrado_exponencial = rse_exponencial(data.dia, data.acumulados, a_exponencial, b_exponencial)
+print("El error obtenido es: ", error_cuadrado_exponencial)
+print("El r obtenido es de: ", r_obtenido_exponencial, "\n")
+
+
+
+
+
+
+"""
 b = 1
 a = 1
 y = b*x**a
@@ -119,4 +174,4 @@ p = np.polyfit(data.dia, np.log(y), 1)
 plt.scatter(data.dia, data.acumulados)
 plt.plot(data.dia, funcion_1(data.dia))
 plt.show()
-
+"""
